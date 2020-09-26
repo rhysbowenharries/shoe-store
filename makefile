7f5c84f3c085b0aff0d7ffcf4196194b086f2c58
Rhys Bowen Harries <rhys@optimiseweb.co.uk>
2020-09-26 20:53:52 +0100

    form built

--- makefile
+++ makefile
@@ -2,3 +2,5 @@ git:
 	git add .
 	git commit -m "$m"
 	git push -u origin master
+
+	# in the terminal type: make git m="your message"
--- src/components/shoes/ShoeCreate.js
+++ src/components/shoes/ShoeCreate.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 
+const brands = ["Nike", "Hushpuppy", "Mona Lisa", "Kickers"];
+const categories = [
+  "M sports",
+  "M evening",
+  "M casual",
+  "M slippers",
+  "M Sandles",
+  "W sports",
+  "W evening",
+  "W casual",
+  "W slippers",
+  "Heels",
+  "Pumps",
+];
+
 class ShoeCreate extends React.Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, type }) {
     return (
       <div className="field">
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} type={type} />
       </div>
     );
   }
@@ -19,6 +34,40 @@ class ShoeCreate extends React.Component {
           component={this.renderInput}
           label="Enter Description"
         />
+        <Field
+          name="price"
+          type="number"
+          component={this.renderInput}
+          label="Enter Price"
+        />
+        <Field
+          name="quantity"
+          type="number"
+          component={this.renderInput}
+          label="Enter Quantity"
+        />
+        <div className="field">
+          <label>Enter category</label>
+          <Field name="category" component="select">
+            <option value=""></option>
+            {categories.map((categoryOption) => (
+              <option value={categoryOption} key={categoryOption}>
+                {categoryOption}
+              </option>
+            ))}
+          </Field>
+        </div>
+        <div className="field">
+          <label>Enter Brand</label>
+          <Field name="brand" component="select">
+            <option value=""></option>
+            {brands.map((brandOption) => (
+              <option value={brandOption} key={brandOption}>
+                {brandOption}
+              </option>
+            ))}
+          </Field>
+        </div>
       </form>
     );
   }
