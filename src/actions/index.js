fc6089923ec01c539b6eb82a0e28c2d6ef76355c
Rhys Bowen Harries <rhys@optimiseweb.co.uk>
2020-09-27 13:16:42 +0100

    action creators completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import Localhost from "../apis/localhost:8000";
 import shoesReducer from "../reducers/shoesReducer";
-import { SIGN_IN, SIGN_OUT, FETCH_SHOES } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  FETCH_SHOES,
+  CREATE_SHOE,
+  FETCH_SHOE,
+  DELETE_SHOE,
+  EDIT_SHOE,
+} from "./types";
 
 export const signIn = () => {
   return {
@@ -19,6 +27,26 @@ export const fetchShoes = () => async (dispatch) => {
   dispatch({ type: FETCH_SHOES, payload: response.data });
 };
 
+export const fetchShoe = (id) => async (dispatch) => {
+  const response = await Localhost.get(`/shoes/${id}`);
+
+  dispatch({ type: FETCH_SHOE, payload: response.data });
+};
+
 export const createShoe = (formValues) => async (dispatch) => {
-  Localhost.post("/shoes", formValues);
+  const response = await Localhost.post("/shoes", formValues);
+
+  dispatch({ type: CREATE_SHOE, payload: response.data });
+};
+
+export const editShoe = (id, formValues) => async (dispatch) => {
+  const response = await Localhost.put(`/shoes/${id}`, formValues);
+
+  dispatch({ type: EDIT_SHOE, payload: response.data });
+};
+
+export const deleteShoe = (id) => async (dispatch) => {
+  await Localhost.delete(`/shoes/${id}`);
+
+  dispatch({ type: DELETE_SHOE, payload: id });
 };
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,7 @@
 export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUT";
 export const FETCH_SHOES = "FETCH_SHOES";
+export const FETCH_SHOE = "FETCH_SHOE";
+export const CREATE_SHOE = "CREATE_SHOE";
+export const DELETE_SHOE = "DELETE_SHOE";
+export const EDIT_SHOE = "EDIT_SHOE";
