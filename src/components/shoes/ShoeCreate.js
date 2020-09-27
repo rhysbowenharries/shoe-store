32a91282c258a6572df601670a2a6b43f9f96fcc
Rhys Bowen Harries <rhys@optimiseweb.co.uk>
2020-09-27 10:33:07 +0100

    form validation working

--- src/components/shoes/ShoeCreate.js
+++ src/components/shoes/ShoeCreate.js
@@ -17,17 +17,39 @@ const categories = [
 ];
 
 class ShoeCreate extends React.Component {
-  renderInput({ input, label, type }) {
+  renderInput({ input, label, type, meta }) {
+    console.log(meta);
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} type={type} />
+        <div>{meta.error}</div>
       </div>
     );
   }
+
+  renderSelectInput({ input, label, type, meta, children }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <select {...input} type={type}>
+          {children}
+        </select>
+        <div>{meta.error}</div>
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
   render() {
     return (
-      <form className="ui form">
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
         <Field name="title" component={this.renderInput} label="Enter Name" />
         <Field
           name="description"
@@ -46,33 +68,63 @@ class ShoeCreate extends React.Component {
           component={this.renderInput}
           label="Enter Quantity"
         />
-        <div className="field">
-          <label>Enter category</label>
-          <Field name="category" component="select">
-            <option value=""></option>
-            {categories.map((categoryOption) => (
-              <option value={categoryOption} key={categoryOption}>
-                {categoryOption}
-              </option>
-            ))}
-          </Field>
-        </div>
-        <div className="field">
-          <label>Enter Brand</label>
-          <Field name="brand" component="select">
-            <option value=""></option>
-            {brands.map((brandOption) => (
-              <option value={brandOption} key={brandOption}>
-                {brandOption}
-              </option>
-            ))}
-          </Field>
-        </div>
+
+        <Field
+          name="category"
+          label="Enter Category"
+          component={this.renderSelectInput}
+        >
+          <option value=""></option>
+          {categories.map((categoryOption) => (
+            <option value={categoryOption} key={categoryOption}>
+              {categoryOption}
+            </option>
+          ))}
+        </Field>
+
+        <Field
+          name="brand"
+          label="Enter Brand"
+          component={this.renderSelectInput}
+        >
+          <option value=""></option>
+          {brands.map((brandOption) => (
+            <option value={brandOption} key={brandOption}>
+              {brandOption}
+            </option>
+          ))}
+        </Field>
+
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
 }
 
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "Please Enter A Name ";
+  }
+  if (!formValues.description) {
+    errors.description = "Please Enter A Description";
+  }
+  if (isNaN(Number(formValues.price))) {
+    errors.price = "Please Enter A Price";
+  }
+  if (isNaN(Number(formValues.quantity))) {
+    errors.quantity = "Please Select a quantity";
+  }
+  if (isNaN(Number(formValues.price))) {
+    errors.category = "Please Select a Category";
+  }
+  if (!formValues.brand) {
+    errors.brand = "Please Select a Brand";
+  }
+  return errors;
+};
+
 export default reduxForm({
   form: "shoeCreate",
+  validate,
 })(ShoeCreate);
